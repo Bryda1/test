ac08eca8f5c6782cd51ebaaee1cbec188cebd41d
Bryda1 <78613500+Bryda1@users.noreply.github.com>
2021-03-21 04:25:21 +0300

    Add files via upload

---
+++ map5/javsScript/javaScript.js
@@ -0,0 +1,19 @@
+
+
+/*
+
+
+function CreateBlocks() {
+    for (let i = 1; i <= 0; i++) {
+        $('#target').append($('<div/>', { id: 'r' + i, 'class': 'square' }))
+     
+    }
+}
+CreateBlocks()
+
+
+*/
+
+
+
+
